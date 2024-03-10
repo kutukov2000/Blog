f97ad1f3487193c166b9cf8defef2094ef9ccfa1
kutukov2000 <kutukov16.2005@gmail.com>
2024-03-10 16:07:16 +0200

    feat: register ✨

--- src/main/java/com/example/controllers/AccountController.java
+++ src/main/java/com/example/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.example.controllers;
 import lombok.RequiredArgsConstructor;
 import com.example.dtos.account.AuthResponseDTO;
 import com.example.dtos.account.LoginDTO;
+import com.example.dtos.account.RegisterDTO;
 import com.example.services.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/account")
 @RequiredArgsConstructor
 public class AccountController {
-    private final AccountService service;
+    private final AccountService accountService;
 
     @PostMapping("login")
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO dto) {
         try {
-            var auth = service.login(dto);
+            var auth = accountService.login(dto);
+            return ResponseEntity.ok(auth);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterDTO dto) {
+        try {
+            var auth = accountService.register(dto);
             return ResponseEntity.ok(auth);
         } catch (Exception ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
--- src/main/java/com/example/services/AccountService.java
+++ src/main/java/com/example/services/AccountService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import com.example.configuration.security.JwtService;
 import com.example.dtos.account.AuthResponseDTO;
 import com.example.dtos.account.LoginDTO;
+import com.example.dtos.account.RegisterDTO;
+import com.example.entities.UserEntity;
 import com.example.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +26,27 @@ public class AccountService {
             throw new UsernameNotFoundException("User not found");
         }
         var jwtToken = jwtService.generateAccessToken(user);
+        return AuthResponseDTO.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthResponseDTO register(RegisterDTO request) {
+        if (userRepository.findByEmail(request.getEmail()).orElse(null) != null) {
+            throw new IllegalArgumentException("Email already registered");
+        }
+
+        UserEntity newUser = UserEntity.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .phone(request.getPhone())
+                .build();
+
+        userRepository.save(newUser);
+
+        var jwtToken = jwtService.generateAccessToken(newUser);
         return AuthResponseDTO.builder()
                 .token(jwtToken)
                 .build();
