07ed2cf913f503366e9eecbf55f22ad7470e98e4
kutukov2000 <kutukov16.2005@gmail.com>
2024-03-23 13:23:20 +0200

    feat: category getById ✨

--- src/main/java/com/example/controllers/CategoryController.java
+++ src/main/java/com/example/controllers/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +36,18 @@ public class CategoryController {
         return new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<CategoryItemDTO> getById(@PathVariable int id) {
+        CategoryEntity category = categoriesRepository.findById(id).orElse(null);
+
+        if (category == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        CategoryItemDTO postItemDTO = categoryMapper.categoryItemDTO(category);
+        return new ResponseEntity<>(postItemDTO, HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<CategoryItemDTO> create(CategoryCreateDTO categoryCreateDTO) {
         try {
@@ -48,7 +61,7 @@ public class CategoryController {
     }
 
     @PutMapping()
-    public ResponseEntity<CategoryItemDTO> edit(CategoryEditDTO updatedCategoryDTO) {
+    public ResponseEntity<CategoryItemDTO> edit(@RequestBody CategoryEditDTO updatedCategoryDTO) {
         CategoryEntity category = categoriesRepository.findById(updatedCategoryDTO.getId())
                 .orElse(null);
 
