050a4c7650a9c97c050d37dfc731a821325e0953
kutukov2000 <kutukov16.2005@gmail.com>
2024-03-23 16:02:03 +0200

    fix: post create and edit 🐛

--- src/main/java/com/example/controllers/PostController.java
+++ src/main/java/com/example/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,7 +56,7 @@ public class PostController {
     }
 
     @PostMapping
-    public ResponseEntity<PostItemDTO> createPost(PostCreateDTO postCreateDTO) {
+    public ResponseEntity<PostItemDTO> createPost(@RequestBody PostCreateDTO postCreateDTO) {
         try {
             PostEntity postEntity = postMapper.postCreateDTOToEntity(postCreateDTO);
             postsRepository.save(postEntity);
@@ -66,9 +67,9 @@ public class PostController {
         }
     }
 
-    @PutMapping
-    public ResponseEntity<PostItemDTO> editPost(PostEditDTO postEditDTO) {
-        PostEntity existingPost = postsRepository.findById(postEditDTO.getId())
+    @PutMapping("{id}")
+    public ResponseEntity<PostItemDTO> editPost(@PathVariable int id, @RequestBody PostEditDTO postEditDTO) {
+        PostEntity existingPost = postsRepository.findById(id)
                 .orElse(null);
         if (existingPost == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/com/example/dtos/post/PostEditDTO.java
+++ src/main/java/com/example/dtos/post/PostEditDTO.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class PostEditDTO {
-    private int id;
     private String title;
     private String shortDescription;
     private String description;
